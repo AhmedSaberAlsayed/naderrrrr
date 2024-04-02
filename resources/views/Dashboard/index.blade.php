789dba23fd2c972f7f09f0010f3a683a7fd28a72
naderZ2 <naderzakari2@gmail.com>
2024-04-02 23:46:54 +0200

    first commit

---
+++ resources/views/Dashboard/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('Dashboard\layouts\HeadDashboard')
+<body>
+    <div class="container-fluid position-relative d-flex p-0">
+        
+        <!-- Spinner Start -->
+        @include('Dashboard\layouts\Spinner')
+        <!-- Spinner End -->
+
+        <!-- Sidebar Start -->
+        @include('Dashboard\layouts\Sidebar')
+        <!-- Sidebar End -->
+
+        <!-- Content Start -->
+        <div class="content">
+            
+            <!-- Navbar Start -->
+            @include('Dashboard\layouts\Navbar')
+            <!-- Navbar End -->
+
+
+
+                {{-- thecontent --}}
+
+
+                
+            <!-- Footer Start -->
+            @include('Dashboard\layouts\Footer')
+            <!-- Footer End -->
+        </div>
+        <!-- Content End -->
+    </div>
+
+    {{-- js script --}}
+    @include('Dashboard\layouts\JSScript')
+    
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
+    return view('Dashboard/index');
+})->name('dashboardHome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
